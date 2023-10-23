56a42841a9bd39e13a3fa5b16a070c797c1b8268
hyoni-ch <asdfgh9933@daum.net>
2023-10-24 02:47:14 +0900

    프로젝트 내용 개발 중 멘트 수정

--- src/components/projects/Dust.js
+++ src/components/projects/Dust.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Spacer } from "../style/CommonCss";
+import { MainBox } from "../style/MainCss";
 
 export default function Dust() {
   return (
-    <div>
-      <div>프로젝트 세부 내용 개발 중입니다!</div>
-    </div>
+    <MainBox>
+      <Spacer />
+      <div>프로젝트 내용 개발 중입니다!</div>
+    </MainBox>
   );
 }
